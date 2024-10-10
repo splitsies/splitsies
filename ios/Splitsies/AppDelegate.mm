990c484cb68de56d769e929424871f2834468249
Kevin Chen <kevchen21@gmail.com>
2024-10-10 11:26:14 -0700

    Splitsiesapp.com domain linking (#129)
    * add splitsiesapp.com link
    
    * fix url scheme
    
    * use domain linking
    
    * app entitlements
    
    * deployments

--- ios/Splitsies/AppDelegate.mm
+++ ios/Splitsies/AppDelegate.mm
@@ -49,6 +49,14 @@
   return [RCTLinkingManager application:application openURL:url options:options];
 }
 
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
 - (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
                           moduleName:(NSString *)moduleName
                            initProps:(NSDictionary *)initProps {
